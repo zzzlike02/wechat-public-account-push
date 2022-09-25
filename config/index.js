3007b8ce24ad1ad1e7e59f01c4feb80c8bfab596
zzzlike02 <114321518+zzzlike02@users.noreply.github.com>
2022-09-25 10:30:06 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -7,12 +7,12 @@ const USER_CONFIG = {
   // 公众号APP_ID
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_ID: '',
+  APP_ID: 'wxc813579cb169cdbb',
 
   // 公众号APP_SECRET
   // 建议不要填这里，请使用文档中github secret的方法进行保密配置，保护您的隐私安全。
   // 如果你非要填这里也行。脚本也能运行
-  APP_SECRET: '',
+  APP_SECRET: '008722465db2df21385b09e00e774924',
 
   // 是否给文字设置多彩颜色, 和emoji不兼容
   // 如果您使用了微信测试号的模板中含有emoji表情，请填 false
@@ -63,17 +63,17 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: '老婆0',
+      name: '张老师',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oCVOp6JjCWKb7pfoyqM8fB7Z2rQs',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'A5UjC4f1sgrfeqZlJcF3kOKmESDQSvjD3ktKmPiLjyY',
       // 所在省份
-      province: '广东',
+      province: '重庆',
       // 所在城市
-      city: '肇庆',
+      city: '重庆',
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-27',
+      horoscopeDate: '10-08',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
@@ -82,27 +82,27 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '老婆', year: '1996', date: '09-09',
+          type: '生日', name: '张老师', year: '2001', date: '10-08',
         },
         {
-          type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
+        //type: '节日', name: '结婚纪念日', year: '2020', date: '09-03',
         },
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '帅鸽', year: '2002', date: '02-01',
         },
         {
-          type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
+        //type: '节日', name: '被搭讪纪念日', year: '2021', date: '09-01',
         }
       ],
       // 专属纪念日/倒数日，如果你在这里填写了纪念日/倒数日，就不会计算CUSTOMIZED_DATE_LIST的日子了, 和CUSTOMIZED_DATE_LIST的配置方法相同，可以往下查看，我这里就不重复写了
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-09-08' },
+        //{ keyword: 'love_day', date: '2022-09-08' },
         // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
+        //{ keyword: 'marry_day', date: '2022-09-09' },
         // 退伍日
-        { keyword: 'ex_day', date: '2022-09-10' }
+        //{ keyword: 'ex_day', date: '2022-09-10' }
       ],
       // 课程表相关配置
       // 如果courseSchedule不存在或者为空（null）则认为没有课程
@@ -123,38 +123,38 @@ const USER_CONFIG = {
           odd: [
             // 例子，周一的课表
             [
-              '08:00-09:35 高等数学',
-              '09:50-11:25 高等物理'
+              //'08:00-09:35 高等数学',
+              //'09:50-11:25 高等物理'
             ],
             // 周二
             [
-              '08:00-09:35 高等数学',
-              '09:50-11:25 高等物理'
+              //'08:00-09:35 高等数学',
+              //'09:50-11:25 高等物理'
             ],
             // 周三
             [
-              '08:00-09:35 高等数学',
-              '09:50-11:25 高等物理'
+              //'08:00-09:35 高等数学',
+              //'09:50-11:25 高等物理'
             ],
             // 周四
             [
-              '08:00-09:35 高等数学',
-              '09:50-11:25 高等物理'
+              //'08:00-09:35 高等数学',
+              //'09:50-11:25 高等物理'
             ],
             // 周五
             [
-              '08:00-09:35 高等数学',
-              '09:50-11:25 高等物理'
+              //'08:00-09:35 高等数学',
+              //'09:50-11:25 高等物理'
             ],
             // 周六
             [
-              '08:00-09:35 高等数学',
-              '09:50-11:25 高等物理'
+              //'08:00-09:35 高等数学',
+              //'09:50-11:25 高等物理'
             ],
             // 周日
             [
-              '08:00-09:35 高等数学',
-              '09:50-11:25 高等物理'
+              //'08:00-09:35 高等数学',
+              //'09:50-11:25 高等物理'
             ]
           ],
           // 双周课表
@@ -171,16 +171,17 @@ const USER_CONFIG = {
       },
     },
     {
-      name: '老婆1',
-      id: '',
-      useTemplateId: '',
-      province: '',
-      city: '',
-      horoscopeDate: '',
-      horoscopeDateType: '',
+      name: '帅鸽',
+      id: 'oCVOp6Kx3F8LlPX0bgKaERdwuHPI',
+      useTemplateId: 'A5UjC4f1sgrfeqZlJcF3kOKmESDQSvjD3ktKmPiLjyY',
+      province: '云南',
+      city: '大理',
+      horoscopeDate: '02-01',
+      horoscopeDateType: '今日',
       openUrl: 'https://wangxinleo.cn',
-      festivals: [],
-      customizedDateList: [],
+      festivals: [{"type": "生日", "name": "张老师", "year": "2001", "date": "10-08"},
+                 {"type": "生日", "name": "帅鸽", "year": "2002", "date": "02-01"},],
+      //customizedDateList: [],
       courseSchedule: null
     },
     {
@@ -218,7 +219,7 @@ const USER_CONFIG = {
    */
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '',
+  CALLBACK_TEMPLATE_ID: 'lbIO7erqdb0ImI3_U0kT-OlqlSA75g0raSdmqeMWMbM',
 
   // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
   CALLBACK_USERS: [
@@ -226,7 +227,7 @@ const USER_CONFIG = {
       // 一般都填自己
       name: '自己',
       // 自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'oCVOp6Kx3F8LlPX0bgKaERdwuHPI',
     }
     // 你可以不断按格式往下增加
     // ...
@@ -239,9 +240,9 @@ const USER_CONFIG = {
   /** 天气相关 */
 
   // 默认所在省份, USERS 中没填的话, 会默认拿这里的省份
-  PROVINCE: '广东',
+  PROVINCE: '重庆',
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
-  CITY: '惠州',
+  CITY: '重庆',
 
   /** 重要节日相关 */
 
